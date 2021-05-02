e0794fb4043444389292a41a04b09c0fda20121e
Emerson Souza <emersonwebdev@gmail.com>
2021-05-02 10:29:14 -0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import api from './services/api';
+
+interface IntUserData {
+  followers: number;
+  following: number
+}
+
 function App() {
+  const [username, setUsername] = useState('');
+  const [userdata, setUserData] = useState<IntUserData>({ followers: 0, following: 0 });
+
+  async function findGithubUser(username: string) {
+    try {
+      const { data } = await api.get(`/${username}`);
+      setUserData(data);
+    } catch (error) {
+      console.log(error)
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="search">
+          <input
+            className="input"
+            placeholder="Digite o nome do usuário"
+            value={username}
+            onChange={text => setUsername(text.target.value)}
+          />
+          
+          <button className="button" onClick={() => findGithubUser(username)}>Buscar</button>
+        </div>
+        <hr />
+        <hr />
+        <hr />
+        <div>FOLLOWERS: {`${userdata.followers}`}</div>
+        <div>FOLLOWING: {`${userdata.following}`}</div>
       </header>
     </div>
   );
